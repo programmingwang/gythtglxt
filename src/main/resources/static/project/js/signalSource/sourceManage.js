e651803ddc5ba577f33ded7d3062c0979df88a76
programmingwang <64132721+programmingwang@users.noreply.github.com>
2020-12-14 20:39:51 +0800

    12.14

--- src/main/resources/static/project/js/signalSource/sourceManage.js
+++ src/main/resources/static/project/js/signalSource/sourceManage.js
@@ -11,7 +11,9 @@
 
             var opUrl = "/accountSource/signal-source";
 
-
+            if(sessionStorage.getItem("rolename") == "管理员"){
+                $('#btn_addTask').attr('style',"display:block");
+            }
 
             var aParam = {};
 
