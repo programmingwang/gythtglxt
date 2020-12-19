6887eaee5eac84a93e26b1928974700625c9c997
ncn-1017 <ncn_edu@qq.com>
2020-12-19 18:15:10 +0800

    前面提交错了

--- src/test/java/com/gythtglxt/GythtglxtApplicationTests.java
+++ src/test/java/com/gythtglxt/GythtglxtApplicationTests.java
@@ -3,30 +3,11 @@ package com.gythtglxt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 @SpringBootTest
 class GythtglxtApplicationTests {
 
-    public static boolean isPhone(String str) {
-        Pattern p1 = null,p2 = null;
-        Matcher m = null;
-        boolean b = false;
-        p1 = Pattern.compile("^[0][1-9]{2,3}-[0-9]{5,10}$");  // 验证带区号的
-        p2 = Pattern.compile("^[1-9]{1}[0-9]{5,8}$");         // 验证没有区号的
-        if(str.length() >9)
-        {   m = p1.matcher(str);
-            b = m.matches();
-        }else{
-            m = p2.matcher(str);
-            b = m.matches();
-        }
-        return b;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(isPhone("011-1234567"));
+    @Test
+    void contextLoads() {
     }
 
 }
