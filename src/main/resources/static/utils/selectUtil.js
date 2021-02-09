9cde02d1da69de471e8e8f8dce9423e8226f50e3
zuohong <823178351@qq.com>
2021-02-10 01:24:58 +0800

    2.10 搜索

--- src/main/resources/static/utils/selectUtil.js
+++ src/main/resources/static/utils/selectUtil.js
@@ -213,12 +213,12 @@
 
         function inSearchStatus(){
             var status = [
+                { id: "99",text: "全部"},
                 { id: "0",text: "保存"},
                 { id: "1",text: "审核中"},
                 { id: "2",text: "审核未通过"},
                 { id: "3",text: "已上架"},
-                { id: "4",text: "已下架"},
-                { id: "99",text: "全部状态"},
+                { id: "4",text: "已下架"}
             ]
             return status;
         }
